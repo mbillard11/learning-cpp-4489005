5e3a0c6a1be2763096acc158e5054b7d27f2bae7
Mitchell Billard <mbillard11@gmail.com>
2024-11-13 03:09:26 +0000

    Ch2 challenge solution

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -5,10 +5,11 @@
 #include <iostream>
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
+    int nums[5] = {1,2,3,2,3};
     float result; 
 
     // Write your code here
+    result = (nums[0] + nums[1] + nums[2] + nums[3] + nums[4]) / 5.0f;
     
     std::cout << "The average is " << result << std::endl;
     
